947aad5616b3c49bc7b1c61cc2a27f859e174973
romaindepret91 <romaindepret91@gmail.com>
2023-02-03 09:54:35 -0500

    Client: adjustments of design on collapsed content of header menu

--- client/src/components/header/HeaderMenu.jsx
+++ client/src/components/header/HeaderMenu.jsx
@@ -16,7 +16,7 @@ export default function HeaderMenu({
       <Navbar bg="light" expand="lg">
         <Container className="ps-0">
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
+          <Navbar.Collapse id="basic-navbar-nav" className="collapse-content">
             <Nav className="mr-auto pb-1 border-bottom w-100">
               <Nav.Link className="ps-0" href="/">
                 Home
