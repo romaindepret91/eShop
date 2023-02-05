7ceb599945c5a98a78ea8f9401d8cf4594d62e5b
romaindepret91 <romaindepret91@gmail.com>
2023-02-05 17:51:57 -0500

    Client: ProductSheet sizes - select button

--- client/src/components/products/ProductSheetActions.jsx
+++ client/src/components/products/ProductSheetActions.jsx
@@ -2,14 +2,30 @@ import { Container, Button, Col, Row } from "react-bootstrap";
 import "./ProductSheetActions.scss";
 
 export default function ProductSheetActions({ product }) {
+  const handleSelectSize = (e) => {
+    const eltClass = e.currentTarget.className;
+    const sizes =
+      e.currentTarget.parentNode.parentNode.querySelectorAll(".col-2 > button");
+
+    sizes.forEach((element) => {
+      element.className === eltClass
+        ? element.classList.add("active")
+        : element.classList.remove("active");
+    });
+  };
   return (
     <Container className="ProductSheetActions ">
       <Row className="ProductSheetActions-size">
         <Col xs={12}>Select size:</Col>
         {Object.keys(product.stock).map((size, quantity) => {
           return (
-            <Col>
-              <Button variant="light" disabled={quantity < 1}>
+            <Col xs={2} key={size}>
+              <Button
+                variant="light"
+                className={size}
+                disabled={quantity < 1}
+                onClick={(e) => handleSelectSize(e)}
+              >
                 {!size.includes("oz") ? size.toUpperCase() : size}
               </Button>
             </Col>
